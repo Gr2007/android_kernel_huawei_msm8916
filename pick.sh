520f7fb46d4471f31c6401e62361ab3a4b185eab
FireLord1 <firelord.xda@gmail.com>
2016-07-09 22:36:14 +0530

    N-release

---
+++ pick.sh
@@ -0,0 +1,16 @@
+
+git cherry-pick 4ce4a1b5057bc6843cb9dbfcc660bd2ec76d4e59
+git cherry-pick 7ca200a78b345b621d8a4ac5a041d99e5f58c475
+git cherry-pick db6b25785e8aabd1f39268c69f86f05a68a698ba
+git cherry-pick eade467e52182b0e609de3b38b3eae1924fe850c
+git cherry-pick 12659b1c38b3832312bcc4ea25e7cead0843a8b8
+git cherry-pick 5ead7eb36bbc1ddd3664635eac5e6aa04655a1ea
+git cherry-pick 55ffdc3fcd16f663d489d0cd0cb47c995a7eab83
+git cherry-pick 4ce4a1b5057bc6843cb9dbfcc660bd2ec76d4e59
+git cherry-pick cc18ff055755c4d0a8826d6d9bb54d400dbc8475
+git cherry-pick 3d2350cd88673a811ca8ff1a353e92ac2a43e0b1
+git cherry-pick ff8c752b30295463e6bb5676cd886184f6083845
+git cherry-pick 15a3e5c9216e219ee5db15aa4b62140b54ff7126
+git cherry-pick 88dda0ddc062b31ba66303e3193d533c79478292
+git cherry-pick e80e3c80f37548ea93206d0a314d8f7d8209257a
+
