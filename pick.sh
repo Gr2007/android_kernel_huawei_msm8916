2547480f0e928f19cf729cda42b7fd7c77626770
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2016-09-04 21:40:25 -0500

    fix audio

---
+++ pick.sh
@@ -0,0 +1,46 @@
+ #
+ # Copyright � 2016,  Sultan Qasim Khan <sultanqasim@gmail.com> 	
+ # Copyright � 2016,  Zeeshan Hussain <zeeshanhussain12@gmail.com> 	      
+ # Copyright � 2016,  Varun Chitre  <varun.chitre15@gmail.com>	
+ # Copyright � 2016,  Carlos Arriaga  <CarlosArriagaCM@gmail.com>	
+ #
+ # Custom build script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+
+git cherry-pick 25570025aa19eb8579990ad397cbe122f3eff304
+git cherry-pick 1eae1b0f2756037ecc30447b5727c876daff7443
+git cherry-pick fd3c16cde0d5e1badb290418471bee6966a16ffb
+git cherry-pick b15064c568336a02e8a33b229542c8f929e4ffab
+git cherry-pick 553c02944eb8ffd075ea338d03ca90ca62809872
+git cherry-pick ee5fe02d8e37cbbc11b8c9cccc9707f5946b51f2
+git cherry-pick 686dff6902bb64b9bdbbf5b3eda86ca66d761ba5
+git cherry-pick 3ec56a1a9a91c799df22ce44127572652390b89f
+git cherry-pick 3cbc60d732709d3c9d44d90e0a8af874d7e4b833
+git cherry-pick 68b69454d7ae329e5f4db67be7433a9435d455e5
+git cherry-pick f3ddaf5b0a1a216d885c60b44a6f366b94c26f7e
+git cherry-pick 91844e6f8f0c45f811e513b2db05f83a323d3b9a
+git cherry-pick b86216157bfaa12e04590091f8489eaea8e6efd8
+git cherry-pick 13c6976042ed20ffac6ed22bc7233e34affb6226
+git cherry-pick 1577df27231b63b99e85d3022be67474db5f5b12
+git cherry-pick d4462706d7fb8670ec24b5acf1196904463390f5
+git cherry-pick 58feb9c1e7544b4fa7f248fc63a2bd85f89dc41f
+git cherry-pick b721474c4498286fbc6db55695ffe0f31230f55d
+git cherry-pick 2bbc21506cac7ff01d9c72394a6e2e02e877b2d0
+git cherry-pick 0bc0a96062a4b7697736a2e503a8cd2fe91ec66d
+git cherry-pick 9855addaf5363aed7861eb90592c9fb1cb8dfe93
+git cherry-pick 95c7e869f8b756dac15c2616b54f8bd6abbd3241
+git cherry-pick 5b99ddfbdb586d5aad01546d568b15c1c60cb3aa
+git cherry-pick 4e371bd17e93ac5e7796a3c9c56897d2779cb1f4
+git cherry-pick 0b6b960dbfc492a7502bcace69d8f4c99638ea3c
+git cherry-pick f6407b7d87653a08d0956aefb6fbad4ea17361b3
