d187268ae6853592fb5e44404171e072d0899d2a
Gr2007 <gvjohan99@gmail.com>
2016-10-09 01:11:10 -0500

    first commit

--- Gr2.sh
+++ Gr2.sh
@@ -3,7 +3,7 @@
 #Datos
 VERSION="V9"
 zimagen="/home/gr2_srk/msm8916/arch/arm/boot/zImage"
-ZIP="/home/gr2_srk/msm8916/zip/"
+zp="/home/gr2_srk/msm8916/zip/"
 BUILD_START=$(date +"%s")
 blue='\033[0;34m'
 cyan='\033[0;36m'
@@ -12,63 +12,74 @@ green='\033[0;32m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-echo -e "$green Desea limpiar el directorio?"
+#Menu
+
+#Opcion 1
+echo -e "$green Desea limpiar el directorio? (y/n)"
 read REPLY1
-if [[ $REPLY1 =~ ^[Yy]$ ]]
-then
-    echo -e  "$red Borrando directorio"
-    make clean && make mrproper
-    git reset --hard
-#COMPILACION
-    echo -e "$blue Desea compilar el kernel?"
-    read REPLY2
 
-        if [[ $REPLY2 =~ ^[Yy]$ ]]
-        then
-	echo -e "$yellow*****************************************************"
-	echo "                Iniciando Compilación DemoniacTeam         "
-	echo -e "*****************************************************$nocol"
-            export CROSS_COMPILE=/home/gr2_srk/arm-linux-androideabi-5.x/bin/arm-eabi-
-            export ARCH=arm
-            make gr2_defconfig
-            make -j5
-	echo -e "$cyan Creando zip"
-	mv $zimagen $ZIP
-        cd $ZIP
-	zip -r DemonacTeam-Kernel-$VERSION *
-	cd       
-	BUILD_END=$(date +"%s")
-	DIFF=$(($BUILD_END - $BUILD_START))
-	echo -e "$yellow COmpilacion completa en $(($DIFF / 60)) minuto(s) con $(($DIFF % 60)) segundos.$nocol"    
-        fi
+	if [[ $REPLY1 =~ ^[Yy]$ ]]  #INCIO DEL IF
+		then
+   		echo -e  "$red Borrando directorio"
+   		make clean && make mrproper
 
+		#COMPILACION
+
+		#Opcion2
+    		echo -e "$blue Desea compilar el kernel?"
+    		read REPLY2
+
+        	if [[ $REPLY2 =~ ^[Yy]$ ]]
+        	then
+		echo -e "$yellow*****************************************************"
+		echo "                Iniciando Compilación DemoniacTeam         "
+		echo -e "*****************************************************$nocol"
+           	 export CROSS_COMPILE=/home/gr2_srk/arm-linux-androideabi-5.x/bin/arm-eabi-
+            	export ARCH=arm
+            	make gr2_defconfig
+            	make -j5
+		echo -e "$cyan Creando zip"
+		mv $zimagen $zp
+        	cd $zp
+		zip -r DemonacTeam-Kernel-$VERSION *
+		cd       
+		BUILD_END=$(date +"%s")
+		DIFF=$(($BUILD_END - $BUILD_START))
+		echo -e "$yellow COmpilacion completa en $(($DIFF / 60)) minuto(s) con $(($DIFF % 60)) segundos.$nocol"    
+       fi #FIN DEL IF
+
+#CASO CONTRARIO 
 elif [[ $REPLY1 =~ ^[Nn]$ ]]
 then
 
-    echo -e "$blue Desea compilar el kernel?"
-    read REPLY3
+#OPCION 3    
+echo -e "$blue Desea compilar el kernel?"
+read REPLY3
 
-        if [[ $REPLY3 =~ ^[Yy]$ ]]
-        then
-#COMPILACION   
-	echo -e "$yellow*****************************************************"
-	echo "          Iniciando Compilación DemoniacTeam          "
-	echo -e "*****************************************************$nocol"             
-            export CROSS_COMPILE=/home/gr2_srk/arm-linux-androideabi-5.x/bin/arm-eabi-
-            export ARCH=arm
-            make gr2_defconfig
-            make -j5
-	echo -e "$cyan Creando zip"
-	mv $zimagen $ZIP
-        cd $ZIP
-	zip -r DemonacTeam-Kernel-$VERSION *
-	cd       
-	BUILD_END=$(date +"%s")
-	DIFF=$(($BUILD_END - $BUILD_START))
-	echo -e "$yellow COmpilacion completa en $(($DIFF / 60)) minuto(s) con $(($DIFF % 60)) segundos.$nocol"                
-	fi
+        if [[ $REPLY3 =~ ^[Yy]$ ]] #INICO DEL IF
+        	then
+	#COMPILACION   
+		echo -e "$yellow*****************************************************"
+		echo "          Iniciando Compilación DemoniacTeam          "
+		echo -e "*****************************************************$nocol"             
+            	export CROSS_COMPILE=/home/gr2_srk/arm-linux-androideabi-5.x/bin/arm-eabi-
+            	export ARCH=arm
+            	make gr2_defconfig
+            	make -j5
+		echo -e "$cyan Creando zip"
+		mv $zimagen $zp
+        	cd $zp
+		zip -r DemonacTeam-Kernel-$VERSION *
+		cd       
+		BUILD_END=$(date +"%s")
+		DIFF=$(($BUILD_END - $BUILD_START))
+		echo -e "$yellow COmpilacion completa en $(($DIFF / 60)) minuto(s) con $(($DIFF % 60)) segundos.$nocol"                
+	fi #FIN DEL IF
 fi
-	echo -e "$yellow*****************************************************"
-	echo "                       Compilacion completa!         "
-	echo -e "*****************************************************$nocol" 
+
+#MENSAJE
+echo -e "$yellow*****************************************************"
+echo "                       Compilacion completa!         "
+echo -e "*****************************************************$nocol" 
+
 
